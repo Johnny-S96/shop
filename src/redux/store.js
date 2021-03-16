c37e58711bf145c8b944191b9f151f63285ca018
John <jozaman96@gmail.com>
2021-03-16 20:01:03 +0200

    added redux persist to rehidrate cart component

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import {createStore, applyMiddleware} from 'redux';
+import {persistStore} from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reduser';
 
 const middrewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middrewares));
+export const store = createStore(rootReducer, applyMiddleware(...middrewares));
 
-export default store;
+export const persistor = persistStore(store);
