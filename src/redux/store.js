3ec4b90fb4866bdb5caa5b91331123217bea189b
John <jozaman96@gmail.com>
2021-03-22 21:45:28 +0200

    added redux-thunk, added collection overview and collection containers to make page component's code cleaner

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import {createStore, applyMiddleware} from 'redux';
 import {persistStore} from 'redux-persist';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import rootReducer from './root-reduser';
 
-const middrewares = [];
+const middrewares = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
   middrewares.push(logger);
