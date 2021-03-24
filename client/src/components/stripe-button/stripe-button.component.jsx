5b7a3f70624eb859713b56b9044638267bf7749b
John <jozaman96@gmail.com>
2021-03-24 20:03:30 +0200

    added axios lib and updated stripe-button component with backend in order to make test stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({price}) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_51IVyV8KznuWoI3kwZSDYt9AjQz7GWOqwzaKZ9fzeu0DKwpJ9Ck041F0RuPo1vg85rRcKsKbRuLjZ8ANVXcUa2FUg00TR72Qy7X';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    }).then(response => {
+      alert('Payment successful')
+    }).catch(error => {
+      console.log('Payment error: ', JSON.parse(error));
+      alert(
+        'There was an issue with your payment. Please sure you use the provided credid card.'
+      );
+    });
   };
 
   return(
